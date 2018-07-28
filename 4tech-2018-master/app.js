1016e2ac61601de73b1d68c786e3bfbce8784f86
vntlab <vntlab@suportes-MacBook-Pro-6.local>
2018-07-28 10:16:45 -0300

    teste 2

--- 4tech-2018-master/app.js
+++ 4tech-2018-master/app.js
@@ -2,11 +2,11 @@
 
 const server = require('./config/server');
 const express = require('express');
-const port = 3005;
+const port = process.env.PORT || 3005;
 
 server.use('/vjobs', express.static(__dirname + '/app/static'));
 server.get('/', async(req, res) => {
-    return res.redirect('http://localhost:3005/vjobs');
+    return res.redirect('https://tech18.herokuapp.com/vjobs');
 })
 
 server.listen(port, () => {
